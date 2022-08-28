e8d253d11e5370311e1af9878d4b80a8345ad893
qingchuan.yu <qingchuan.yu@tuya.com>
2022-08-28 20:33:04 +0800

    任务d定义

--- test/t1.py
+++ test/t1.py
@@ -1,1 +1,21 @@
-print('hello1')
+import datetime
+import threading
+from apscheduler.schedulers.background import BackgroundScheduler
+
+def job1():
+    print('job1')
+
+
+def job2(x, y):
+    print('job2', x, y)
+
+
+j_scheduler = BackgroundScheduler()
+j_scheduler.start()
+
+j_scheduler.add_job(job1, trigger='interval', seconds=2)
+
+
+
+event = threading.Event()
+event.wait()
