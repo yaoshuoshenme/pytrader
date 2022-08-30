0df99f9db646cb7809787394ac304bc9b028a43d
yhf <946863181@qq.com>
2022-08-31 00:11:22 +0800

    东方财富接口

--- api/eastmoney_api.py
+++ api/eastmoney_api.py
@@ -1,9 +1,10 @@
 from logging import getLogger
+import sys
 import requests
 from http.cookiejar import Cookie, CookieJar, MozillaCookieJar
-from ..common.constant import Constant
+from common.constant import Constant
 import time
-
+from http.client import HTTPConnection
 
 log = getLogger(__name__)
 
@@ -12,17 +13,14 @@ BASE_URL = "http://push2ex.eastmoney.com"
 
 
 class EastMoneyApi(object):
-    
+
     def __init__(self) -> None:
         self.header = {
             "Accept": "*/*",
             "Accept-Language": "zh-CN,zh;q=0.9",
             "Connection": "keep-alive",
-            "Cache-Control":"no-cache",
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Host": "music.163.com",
             "Referer": "http://quote.eastmoney.com",
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.87 Safari/537.36",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
         }
         cookie_jar = MozillaCookieJar(Constant.cookie_path)
         cookie_jar.load()
@@ -32,17 +30,17 @@ class EastMoneyApi(object):
             if cookie.is_expired():
                 cookie_jar.clear()
                 break
-    
-    def request(self, method, path, params={}, default={"code":-1}, custom_cookies={}):
+
+    def request(self, method, path, params={}, default={"code": -1}, custom_cookies={}):
         endpoint = "{}{}".format(BASE_URL, path)
         data = default
-        mk_cookie = lambda k,v:Cookie(
+        mk_cookie = lambda k, v: Cookie(
             version=0,
             name=k,
             value=v,
             port=None,
             port_specified=False,
-            domain="music.163.com",
+            domain="quote.eastmoney.com",
             domain_specified=True,
             domain_initial_dot=False,
             path="/",
@@ -54,10 +52,10 @@ class EastMoneyApi(object):
             comment_url=None,
             rest={},
         )
-        for k,v in custom_cookies.items():
+        for k, v in custom_cookies.items():
             cookie = mk_cookie(k, v)
             self.session.cookies.set_cookie(cookie)
-        
+
         resp = None
         try:
             resp = self._raw_request(method, endpoint, params)
@@ -68,8 +66,7 @@ class EastMoneyApi(object):
             log.error("Path: {}, response: {}".format(path, resp.text[:200]))
         finally:
             return data
-        
-        
+
     def _raw_request(self, method, endpoint, data=None):
         resp = None
         if method == "GET":
@@ -77,7 +74,6 @@ class EastMoneyApi(object):
         elif method == "POST":
             resp = self.session.post(endpoint, params=data, headers=self.header, timeout=DEFAULT_TIMEOUT)
         return resp
-        
 
     def get_stock_code(self):
         """获取股票代码,包括公司名称，上市时间等
@@ -97,7 +93,7 @@ class EastMoneyApi(object):
         """
         pass
 
-    def get_limit_up_pool(self, date:str=None, start=None, end=None):
+    def get_limit_up_pool(self, date: str = None, start=None, end=None):
         """获取涨停池信息
 
         Args:
@@ -107,17 +103,15 @@ class EastMoneyApi(object):
         """
         path = "/getTopicZTPool"
         params = dict(
-            cb = "callbackdata5230927",
-            ut = "7eea3edcaed734bea9cbfc24409ed989",
-            dpt = "wz.ztzt",
-            Pageindex = 0,
-            pagesize = 100,
-            sort = "fbt:asc",
-            date = date,
-            _ = int(round(time.time()*1000))
+            ut="7eea3edcaed734bea9cbfc24409ed989",
+            dpt="wz.ztzt",
+            Pageindex=0,
+            pagesize=100,
+            sort="fbt:asc",
+            date=date,
+            _=int(round(time.time() * 1000))
         )
         return self.request("GET", path, params)
-        
 
     def get_limit_down_pool(self, date=None, start=None, end=None):
         """获取跌停池信息
@@ -128,8 +122,3 @@ class EastMoneyApi(object):
             end (_type_, optional): 结束. Defaults to None.
         """
         pass
-    
-    
-easy_api = EastMoneyApi()
-data = easy_api.get_limit_up_pool('20220830')
-print(data)
--- common/constant.py
+++ common/constant.py
@@ -1,25 +1,23 @@
 import os
+from sys import platform
 import time
 
+
 class Constant:
-    if "XDG_CONFIG_HOME" in os.environ:
-        conf_dir = os.path.join(os.environ["XDG_CONFIG_HOME"], "s-trader")
+    if platform == 'win32':
+        conf_dir = "D:\\trader\\config"
     else:
-        conf_dir = os.path.join(os.path.expanduser("~"), ".s-strader")
+        conf_dir = os.path.join(os.path.expanduser("~"), "trader")
+
+    if not os.path.exists(conf_dir):
+        os.makedirs(conf_dir)
+
     config_path = os.path.join(conf_dir, "config.json")
-    
-    if "XDG_DATA_HOME" in os.environ:
-        dataDir = os.path.join(os.environ["XDG_DATA_HOME"], "s-trader")
-        if not os.path.exists(dataDir):
-            os.mkdir(dataDir)
-        cookie_path = os.path.join(dataDir, "cookie.txt")
-        log_path = os.path.join(dataDir, "trader.log")
-        storage_path = os.path.join(dataDir, "database.json")
-    else:
-        cookie_path = os.path.join(conf_dir, "cookie.txt")
-        log_path = os.path.join(conf_dir, "trader.log")
-        storage_path = os.path.join(conf_dir, "database.json")
+    cookie_path = os.path.join(conf_dir, "cookie.txt")
+    log_path = os.path.join(conf_dir, "trader.log")
+    storage_path = os.path.join(conf_dir, "database.json")
 
-
-
-print(int(round(time.time()*1000)))
+    # 东方财富指标映射
+    east_money_keymap = {
+        'c': '股票代码',
+    }
